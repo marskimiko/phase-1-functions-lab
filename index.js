1335a7ed1bb0cf579766dd4ffb6950c8dffdb559
marisasaunders <marisaksaunders@gmail.com>
2022-01-11 18:21:28 -0500

    commit tree be6807d20738ba2d4610b451a1efaeece4754731

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp >= hq) {
+        return (pickUp - hq);
+    }
+    else if (pickUp < hq) {
+        return (hq - pickUp);
+    }
+}
+
+function distanceFromHqInFeet(pickUp) {
+   return distanceFromHqInBlocks(pickUp) * 264 
+}
+
+//distanceFromHqInFeet(53);
+
+function distanceTravelledInFeet(startBlock, destinationBlock) {
+    if (startBlock > destinationBlock) {
+        return ((startBlock - destinationBlock) * 264)
+    }
+    else if (startBlock < destinationBlock) {
+        return ((destinationBlock - startBlock) * 264)
+    }
+}
+
+//distanceTravelledInFeet(55, 94);
+
+function calculatesFarePrice(startBlock, destinationBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, destinationBlock);
+
+    if (distanceInFeet <= 400){
+        return 0
+    }
+    else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return ((distanceInFeet - 400) * .02)
+    }
+    else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25
+    }
+    else if (distanceInFeet > 2500) {
+        return 'cannot travel that far'
+    }
+}
